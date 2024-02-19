546ccf4aa6a6bab80c6fd08e73093d3cb1c6cbe2
Ashmita Twayana <sushanttwayana1@gmail.com>
2024-02-19 19:27:16 +0545

    model_trainer.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,8 +9,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -51,8 +55,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-    
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_data,test_data=obj.initiate_data_ingestion()
 
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
